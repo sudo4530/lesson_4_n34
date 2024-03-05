5d052313248e4e1e4685803256c2fcf08f12e31b
sudo <pipsudo@gmail.com>
2024-03-05 20:35:42 +0500

    add new models

--- course/models.py
+++ course/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=50)
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    speciality = models.ManyToManyField(Subject)
+    create_date = models.DateField(auto_now=True)
+
+
+
+
+
